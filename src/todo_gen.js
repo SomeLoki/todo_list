1244b90f1795db7278d78795f7f6078201845986
someloki <tenacity686@gmail.com>
2025-07-17 19:28:16 -0700

    Refactor existing gen logic.

--- src/todo_gen.js
+++ src/todo_gen.js
@@ -1,34 +1,51 @@
 const title = ( title ) => ({
   getTitle: () => title,
-  setTitle: ( newTitle ) => ( title = newTitle ),
+  setTitle: ( newTitle ) =>  title = newTitle,
 });
 
 const complete = ( complete = false ) => ({
   getComplete: () => complete,
-  toggleComplete: () => ( complete = !complete),
+  toggleComplete: () =>  complete = !complete,
 });
 
 const dueDate = ( dueDate ) => ({
   getDueDate: () => dueDate,
-  setDueDate: ( newDueDate ) => ( dueDate = newDueDate),
+  setDueDate: ( newDueDate ) =>  dueDate = newDueDate,
 });
 
 const priority = ( priority = "low" ) => ({
   getPriority: () => priority,
-  setPriority: ( newPriority ) => ( priority = newPriority ),
+  setPriority: ( newPriority ) =>  priority = newPriority,
 });
 
 const description = ( description ) => ({
   getDescription: () => description,
-  setDescription: ( newDescription ) => ( description = newDescription ),
+  setDescription: ( newDescription ) =>  description = newDescription,
 });
 
 const notes = ( notes ) => ({
   getNotes: () => notes,
-  setNotes: ( newNotes ) => ( notes = newNotes ),
+  setNotes: ( newNotes ) =>  notes = newNotes,
 });
 
-const checklist = ( checklist = [] ) => ({
+const checklist = ( ...checklist ) => ({
   getChecklist: () => checklist,
-  setChecklist: ( newChecklist ) => ( checklist = newChecklist ),
+  addChecklistItem: ( newChecklistItem ) => checklist.push(newChecklistItem),
+  removeCheckListItem: ( checklistItem ) => checklist = checklist.filter( (item) => item !== checklistItem ),
 });
+
+const objectTest = function() {
+  return {
+    ...title("test-title"),
+    ...dueDate("test-duedate"),
+    ...complete(),
+    ...priority(),
+    ...description("test-description"),
+    ...notes("test-notes"),
+    ...checklist("test", "checklist"),
+  };
+};
+
+const objTest = objectTest();
+
+window.objTest = objTest;
