76f060848e77df947a607dd2043bf99b1d086688
someloki <tenacity686@gmail.com>
2025-08-01 00:55:32 -0700

    Add utility functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import { createProject } from "./project_gen";
+import { createTodoItem } from "./todo_gen";
+import { createChecklistItem } from "./todo_checklist_gen";
 
-window.createProject = createProject
+window.createProject = createProject;
+window.createTodoItem = createTodoItem;
+window.createChecklistItem = createChecklistItem;
+
+console.log([createProject(), "createProject"]);
+console.log([createProject("test"), "checklist"]);
+
+console.log([createTodoItem(), "createTodoItem"]);
+console.log([createTodoItem("test"), "createTodoItem"]);
+
+console.log([createChecklistItem(), "checklist"]);
+console.log([createChecklistItem("test"), "checklist"]);
